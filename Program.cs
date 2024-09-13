956b90405af82fe01111e9fe714da7f04aa3b0dc
kemtch19 <kemtch19@gmail.com>
2024-09-13 12:27:44 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/* using System.Net.WebSockets;
+
+int num1=3;
+int num2=4;
+
+int sum = num1 + num2;
+Console.WriteLine(sum); */
+
+/* Otra forma */
+Console.Write("Ingresa el número uno: ");
+string num1 = Console.ReadLine();
+
+int num1Parseado;
+int.TryParse(num1, out num1Parseado);
+
+Console.Write("Ingresa el número dos: ");
+string num2 = Console.ReadLine();
+
+int num2Parseado;
+int.TryParse(num2, out num2Parseado);
+
+int sum = num1Parseado+num2Parseado;
+
+Console.WriteLine($"La suma de {num1Parseado} y {num2Parseado} es {sum}");
